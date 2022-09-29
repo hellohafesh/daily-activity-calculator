8445d7a40c6baec74990352f2c6d077a269e69f2
soumik825 <allhafesh825@gmail.com>
2022-09-29 15:35:48 +0600

    added activity component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import Activity from './components/Activity/Activity';
 
 function App() {
   return (
-      );
+    <div className="App">
+      <Activity></Activity>
+    </div>
+  );
 }
 
 export default App;
