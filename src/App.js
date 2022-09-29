f5163d2bb117e204a88d26379748a074b7423a33
soumik825 <allhafesh825@gmail.com>
2022-09-29 15:32:07 +0600

    install React apps

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import Activity from './components/Activity/Activity';
 
 function App() {
   return (
-    <div className="App">
-      <Activity></Activity>
-    </div>
-  );
+      );
 }
 
 export default App;
